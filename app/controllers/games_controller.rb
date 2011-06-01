0ed47e60d39a5bd65f8672d78ba7038b7ee32ac5
nikolaiwarner <n@nikolaiwarner.com>
2011-06-01 17:50:30 -0400

    adding game controller and views

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,83 @@
+class GamesController < ApplicationController
+
+  def index
+    @games = Game.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @games }
+    end
+  end
+
+  # GET /games/1
+  # GET /games/1.json
+  def show
+    @game = Games.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @game }
+    end
+  end
+
+  # GET /games/new
+  # GET /games/new.json
+  def new
+    @game = Games.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @game }
+    end
+  end
+
+  # GET /games/1/edit
+  def edit
+    @game = Games.find(params[:id])
+  end
+
+  # POST /games
+  # POST /games.json
+  def create
+    @game = Games.new(params[:game])
+
+    respond_to do |format|
+      if @game.save
+        format.html { redirect_to @game, notice: 'Game was successfully created.' }
+        format.json { render json: @game, status: :created, location: @game }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /games/1
+  # PUT /games/1.json
+  def update
+    @game = Games.find(params[:id])
+
+    respond_to do |format|
+      if @game.update_attributes(params[:game])
+        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /games/1
+  # DELETE /games/1.json
+  def destroy
+    @game = Games.find(params[:id])
+    @game.destroy
+
+    respond_to do |format|
+      format.html { redirect_to games_url }
+      format.json { head :ok }
+    end
+  end
+
+end
